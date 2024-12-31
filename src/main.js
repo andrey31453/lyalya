f14b0c7a94436cfecb4a8dc16fd818bf96aff1d2
andrey13453 <andrey13453@gmail.com>
2024-12-31 13:59:19 +0300

    -

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-import "./assets/main.css";
-import "primeicons/primeicons.css";
+import './assets/main.css'
 
-import { createApp } from "vue";
-import PrimeVue from "primevue/config";
-import App from "./App.vue";
-import Aura from "@/presets/aura";
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import App from './App.vue'
+import Aura from '@/presets/aura'
 
 createApp(App)
-  .use(PrimeVue, {
-    unstyled: true,
-    pt: Aura,
-  })
-  .mount("#app");
+	.use(PrimeVue, {
+		unstyled: true,
+		pt: Aura,
+	})
+	.mount('#app')
